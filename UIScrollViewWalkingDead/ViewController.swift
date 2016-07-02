327a4dcc1cad0e4a9a179f6213c75cab8f199d43
Yun Shen <yunshenmushroom@gmail.com>
2016-07-02 14:11:04 -0400

    added files for UIScrewView

--- UIScrollViewWalkingDead/ViewController.swift
+++ UIScrollViewWalkingDead/ViewController.swift
@@ -9,17 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView! //var
+    
+    let WIDTH: CGFloat = 416
+    let HEIGHT : CGFloat = 426
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        for var x = 1; x <= 5; x += 1 {
+            let img = UIImage(named: "\(x)")
+            let imgView = UIImageView(image: img)
+            
+            scrollView.addSubview(imgView)//scrollView-subview
+            
+            imgView.frame = CGRectMake(-WIDTH + (WIDTH * CGFloat(x)), 280, WIDTH, HEIGHT)//之前喂怪兽也有用到 (0，270)(240,270)(480,270)
+        }
+        
+        scrollView.contentSize = CGSizeMake(WIDTH * 5, scrollView.frame.size.height)//总宽，scrollview本来高度270
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    
 }
 
